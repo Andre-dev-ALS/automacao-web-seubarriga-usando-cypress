1b3134b7a2f6e3f9be8ee7072feb4f05691feca1
Andre-dev-ALS <andreluizsilvasilva2019@gmail.com>
2024-09-03 17:15:56 -0300

    atualização da estrutura e correção de formatação

--- cypress/support/business/login/loginSteps.js
+++ cypress/support/business/login/loginSteps.js
@@ -1,29 +1,27 @@
-/* global Given, Then, When */
-Cypress.on('uncaught:exception', () => false)
-
 import { Given, Then, When, And } from "cypress-cucumber-preprocessor/steps"
-import loginLogic from '../login/loginLogic';
+import LoginLogic from '../login/loginLogic';
+
 
 Given("que o usuário acessa o site do seubarrigareact", () => {
-  loginLogic.acessarAplicacao();
+  LoginLogic.acessarAplicacao();
 }
 )
 
 When("preencho o campo email", () => {
-  loginLogic.preencherCampoEmail();
+  LoginLogic.preencherCampoEmail();
 }
 )
 
 When("preencho o campo senha", () => {
-  loginLogic.preencherCampoSenha();
+  LoginLogic.preencherCampoSenha();
 }
 )
 
 When("clico em Entrar", () => {
-  loginLogic.clicarEmEntrar();
+  LoginLogic.clicarEmEntrar();
 })
 
 Then("o usuário é redirecionado  a tela inicial já logado", () => {
-  loginLogic.validarUsuarioLogado();
+  LoginLogic.validarUsuarioLogado();
 })
 
